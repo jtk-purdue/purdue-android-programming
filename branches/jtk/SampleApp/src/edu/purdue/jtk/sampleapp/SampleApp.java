308f8b79e114ac18bbe30ea5e2074994e44108fd
jtkorb <jtkorb@715a4810-1644-11df-99ae-394c9fd5fbd1>
2010-02-11 23:10:18 +0000

    Updated, with toast.

--- branches/jtk/SampleApp/src/edu/purdue/jtk/sampleapp/SampleApp.java
+++ branches/jtk/SampleApp/src/edu/purdue/jtk/sampleapp/SampleApp.java
@@ -2,11 +2,13 @@ package edu.purdue.jtk.sampleapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class SampleApp extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Toast.makeText(this, "hello there", Toast.LENGTH_LONG).show();
     }
 }
