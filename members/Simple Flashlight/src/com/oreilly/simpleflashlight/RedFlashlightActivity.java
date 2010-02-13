3041b94b86a63657ee6641dda2596794e21319ed
jtk@cs.purdue.edu <jtk@cs.purdue.edu@715a4810-1644-11df-99ae-394c9fd5fbd1>
2010-02-13 00:01:35 +0000

    Minor change, just to test the Subclipse configuration.

---
+++ members/Simple Flashlight/src/com/oreilly/simpleflashlight/RedFlashlightActivity.java
@@ -0,0 +1,13 @@
+package com.oreilly.simpleflashlight;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RedFlashlightActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
